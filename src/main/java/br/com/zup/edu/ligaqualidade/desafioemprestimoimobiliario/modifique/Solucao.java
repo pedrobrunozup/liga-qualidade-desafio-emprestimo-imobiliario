fe22f0ec39428495a4ae0f0731e0d286f748bbd3
pedrobrunozup <62298482+pedrobrunozup@users.noreply.github.com>
2021-03-06 13:55:20 -0300

    Update Solucao.java

--- src/main/java/br/com/zup/edu/ligaqualidade/desafioemprestimoimobiliario/modifique/Solucao.java
+++ src/main/java/br/com/zup/edu/ligaqualidade/desafioemprestimoimobiliario/modifique/Solucao.java
@@ -1,10 +1,21 @@
 package br.com.zup.edu.ligaqualidade.desafioemprestimoimobiliario.modifique;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Solucao {
 	
   public static String processMessages(List<String> messages) {
-	  return ""; 
+      ListaDePropostas listaDePropostas = new ListaDePropostas(new ArrayList<>());
+      StringBuilder propostasValidas = new StringBuilder();
+      messages.forEach(s -> ProcessaEventos.processarEventos(s, listaDePropostas));
+      listaDePropostas.setListaDePropostas(listaDePropostas.getListaDePropostas().stream().filter(Proposta::isPropostaValida).collect(Collectors.toList()));
+      for (int i=0; i < listaDePropostas.getListaDePropostas().size();i++) {
+        propostasValidas.append(listaDePropostas.getListaDePropostas().get(i).getIdProposta());
+        propostasValidas.append(",");
+      }
+      propostasValidas.deleteCharAt(propostasValidas.lastIndexOf(","));
+      return propostasValidas.toString();
   }
 }
